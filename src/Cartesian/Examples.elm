9dc8279e3569eda8d67bde4dc711a485a26e4198
Christian Berentsen <jc.berentsen@gmail.com>
2021-09-16 11:28:57 +0200

    Try to put illustration in docs

--- src/Cartesian/Examples.elm
+++ src/Cartesian/Examples.elm
@@ -2,6 +2,7 @@ module Cartesian.Examples exposing (basicCell, a, b, c, d, abc, axb, bxa, axb_cx
 
 {-| Example Cartesian diagrams with String labels
 
+# ![elm-wiring-diagrams](https://github.com/jcberentsen/elm-wiring-diagrams/blob/main/assets/png/bypass.png?raw=true)
 
 ## Usage
 
