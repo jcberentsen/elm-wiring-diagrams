aa75cb55e0978b7c0b813c0671f3b835a13d1cea
Christian Berentsen <jc.berentsen@gmail.com>
2021-09-16 11:46:10 +0200

    Try to control image width

--- src/Cartesian/Examples.elm
+++ src/Cartesian/Examples.elm
@@ -2,7 +2,8 @@ module Cartesian.Examples exposing (basicCell, a, b, c, d, abc, axb, bxa, axb_cx
 
 {-| Example Cartesian diagrams with String labels
 
-# ![elm-wiring-diagrams](https://github.com/jcberentsen/elm-wiring-diagrams/blob/main/assets/png/bypass.png?raw=true)
+# ![elm-wiring-diagrams](https://github.com/jcberentsen/elm-wiring-diagrams/blob/main/assets/png/logo.png?raw=true)
+# <img src="https://github.com/jcberentsen/elm-wiring-diagrams/blob/main/assets/png/bypass.png?raw=true" width="600">
 
 ## Usage
 
